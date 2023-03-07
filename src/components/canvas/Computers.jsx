d5315d005fe6cb347f61812d711816cee0b15c50
anggriawanmr <anggriawanmr@gmail.com>
2023-03-07 12:11:15 +0700

    Editing Computers canvas component :hammer:

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,23 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  return <div>Computers</div>;
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas frameloop="demand" shadows camera>
+      {' '}
+    </Canvas>
+  );
 };
 
 export default Computers;
