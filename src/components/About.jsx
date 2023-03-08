6736cdadb34f38778af56cdb175c9faf6a1f636a
anggriawanmr <anggriawanmr@gmail.com>
2023-03-08 12:31:59 +0700

    Adding content in About component :sparkles:

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Full-stack developer with a passion for creating clean, efficient, and
+        scalable web applications. Experienced in using a variety of
+        technologies, including React, Node.js, and PHP, to build dynamic and
+        engaging user experiences. Always looking to learn and grow as a
+        developer, and constantly seeking out new challenges and opportunities
+        to improve my skills.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
