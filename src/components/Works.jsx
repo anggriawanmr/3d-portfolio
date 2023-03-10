f42bba51cc1cc63668e17767c226e15e58b984a0
anggriawanmr <anggriawanmr@gmail.com>
2023-03-10 10:39:33 +0700

    Editing Work section :hammer:

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils';
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Projects</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
 
-export default Works
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn('', '', 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Welcome to my Projects section, where I am thrilled to showcase some
+          of the examples of my completed work. Throughout my learning, I have
+          had the pleasure of working on a diverse range of projects. Each
+          project has presented its own unique challenges and opportunities, and
+          I have always approached them with passion and dedication. In this
+          section, you will find a curated selection of my completed projects
+          that I am particularly proud of. I hope that these examples will
+          provide you with a glimpse of my skills, creativity, and attention to
+          detail, and inspire you to work with me on your next project.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, '');
