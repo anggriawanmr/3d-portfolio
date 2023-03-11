9afdbdc2045d6154841a4925fb2159119a213fdd
anggriawanmr <anggriawanmr@gmail.com>
2023-03-11 12:53:51 +0700

    Adding importing files to Contact component :sparkles:

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  return <div>Contact</div>;
+};
 
-export default Contact
+export default Contact;
