f2a1b64073c65f20e21bd5771bafb90f778dc4e4
anggriawanmr <anggriawanmr@gmail.com>
2023-03-13 11:38:30 +0700

    Adding Heading in Contact component :sparkles:

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,17 @@ const Contact = () => {
 
   const handleSubmit = (e) => {};
 
-  return <div>Contact</div>;
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
+  );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, 'contact');
