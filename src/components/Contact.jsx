0c109c793a79224d4a67127b2e36ea31f60eb442
anggriawanmr <anggriawanmr@gmail.com>
2023-03-14 11:05:08 +0700

    Adding button :sparkles:

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -67,6 +67,13 @@ const Contact = () => {
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg border-none font-medium"
             />
           </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
         </form>
       </motion.div>
     </div>
