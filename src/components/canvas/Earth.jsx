484ad9ac1aab46f19c0a54dd10962cf7794b5268
anggriawanmr <anggriawanmr@gmail.com>
2023-03-14 11:17:38 +0700

    Finalizing Earth component :hammer:

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,11 +1,15 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF('./planet/scene.gltf');
+
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
 };
 
 const EarthCanvas = () => {
@@ -14,7 +18,7 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
