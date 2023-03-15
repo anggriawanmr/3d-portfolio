9a71c80b69cdf15b9647367f509f46809c611c28
anggriawanmr <anggriawanmr@gmail.com>
2023-03-15 13:03:07 +0700

    Finalizing Stars component :hammer:

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -8,8 +8,13 @@ const Stars = (props) => {
 
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
   return (
-    <group>
+    <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
@@ -25,7 +30,7 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className="w-full h-full absolute inset-0 z-[-1]">
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
